c7770f6db5c905d49dc1065b097e6b8b98a6d6c6
birdroid <birdroid@126.com>
2013-09-13 11:47:13 +0800

    简化了消息的解析

--- src/com/eplian/wechat/core/message/receive/RecvMsg.java
+++ src/com/eplian/wechat/core/message/receive/RecvMsg.java
@@ -7,28 +7,25 @@ package com.eplian.wechat.core.message.receive;
  * Time: 下午11:29
  * To change this template use File | Settings | File Templates.
  */
-import com.eplian.wechat.core.message.Msg;
 import com.eplian.wechat.core.message.reply.RplMsg;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.Iterator;
 
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
-public class RecvMsg extends Msg{
+import org.jdom2.Attribute;
+
+public class RecvMsg {
     /**
      * content, such as text, image url, link etc
      */
-    private String content;
-    private String picUrl;
-    /**
-     * session 保存回话信息
-     */
-    public Map<String,String> session=new HashMap<String,String>();
+    public Map<String,String> map=new HashMap<String,String>();
 
     public RecvMsg(InputStream is)throws IOException,JDOMException{
         SAXBuilder saxBuilder = new SAXBuilder();
@@ -36,36 +33,22 @@ public class RecvMsg extends Msg{
         this.parseMsg(document);
     }
 
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
     private void parseMsg(Document document){
-        Element rootElement = document.getRootElement();
-        id = rootElement.getChildTextTrim("MsgId");
-        toUser = rootElement.getChildTextTrim("ToUserName");
-        fromUser = rootElement.getChildTextTrim("FromUserName");
-        msgType = rootElement.getChildTextTrim("MsgType");
-        content = rootElement.getChildTextTrim("Content");
-        createDt = rootElement.getChildTextTrim("CreateTime");
-        picUrl =  rootElement.getChildTextTrim("PicUrl");
-        //event message
-        if ("event".equals(msgType) ){
-            String event = rootElement.getChildText("EventKey");
-            String eventKey = rootElement.getChildText("EventKey");
-            msgType = event;
-            content = eventKey;
+        Element root = document.getRootElement();
+        Iterator it_attr = root.getAttributes().iterator();
+        while (it_attr.hasNext()) {
+            Attribute attribute = (Attribute) it_attr.next();
+            String attrname = attribute.getName();
+            String attrvalue = root.getAttributeValue(attrname);
+            map.put( attrname, attrvalue);
         }
     }
     /**
-     * get reply message
+     * 获取消息的值
+     * @param key
+     * @return
      */
-    public RplMsg buildRplMsg() {
-        return new RplMsg(this.fromUser,this.toUser,
-                (System.currentTimeMillis() / 1000)+"","",false);
+    public String get(String key){
+        return map.get(key);
     }
 }
